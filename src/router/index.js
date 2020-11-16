81b357a5a58556457d3f6c4a72bf9530191796ec
martinalanis <martinalanis.dev@gmail.com>
2020-11-16 17:26:57 -0600

    Rutas protegidas

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '*',
+    redirect: '/login'
+  },
+  {
     path: '/',
     component: () => import('@/layouts/Default'),
     children: [
       {
-        name: 'dashboard',
         path: '',
+        redirect: '/dashboard'
+      },
+      {
+        name: 'dashboard',
+        path: '/dashboard',
         component: () => import('@/views/Dashboard')
       },
       {
@@ -23,11 +32,14 @@ const routes = [
         path: '/refacciones',
         component: () => import('@/views/Refacciones')
       }
-    ]
+    ],
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/login',
-    name: 'Login',
+    name: 'login',
     component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
   }
 ]
@@ -36,6 +48,44 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const isLogged = store.getters['auth/isLogged']
+  console.log(isLogged)
+  const authorization = to.matched.some(record => record.meta.requiresAuth)
+
+  // if (authorization && isLogged) {
+  //   // manda a login si se quiere ingresar a ruta protegida sin estar logueado
+  //   next()
+  // } else if (authorization && !isLogged) {
+  //   // envia a home si ya esta el usuario y se intenta ingresar a login
+  //   next({ name: 'login' })
+  // } else if (!authorization && isLogged) {
+  //   /**
+  //    * Dos posibles casos:
+  //    * Se quiere ingresar a ruta protegida y se esta logueado
+  //    * se quiere ingresar a login y no se esta logueado
+  //    */
+  //   next({ name: 'dashboard' })
+  // } else {
+  //   next({ name: 'login' })
+  // }
+  if (authorization && !isLogged) {
+    // manda a login si se quiere ingresar a ruta protegida sin estar logueado
+    next({ name: 'login' })
+  } else if (!authorization && isLogged) {
+    // envia a home si ya esta el usuario y se intenta ingresar a login
+    next('dashboard')
+  } else {
+    /**
+     * Dos posibles casos:
+     * Se quiere ingresar a ruta protegida y se esta logueado
+     * se quiere ingresar a login y no se esta logueado
+     */
+    console.log(to)
+    next()
+  }
 })
 
 export default router
